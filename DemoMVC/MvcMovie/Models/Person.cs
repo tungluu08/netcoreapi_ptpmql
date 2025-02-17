850f3ddbe90bca1ce14a39f542cf71d13bbd822e
“tungluu08” <luunhattung@gmail.com>
2025-02-17 10:39:48 +0700

    nopbtbosung

---
+++ DemoMVC/MvcMovie/Models/Person.cs
@@ -0,0 +1,10 @@
+namespace MvcMovie.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        
+    }
+}
--- DemoMVC/MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ DemoMVC/MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d45968eb9a593896aa568765c7a10aa2ee9bb752")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47711455c5df410653abc0f27aa5a120cefa3d89")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
